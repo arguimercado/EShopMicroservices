2b0870b3abb8538f2c8ed2e888eb7032ed74df7a
amercado <arnold.mercado@hotmail.com>
2024-07-28 03:07:22 +0300

    Add CQRS in Shared

---
+++ src/Shared/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using FluentResults;
+using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface ICommand : ICommand<Unit>
+{
+
+}
+public interface ICommand<TResponse> : IRequest<Result<TResponse>>
+{
+}
---
+++ src/Shared/BuildingBlocks/CQRS/IQuery.cs
@@ -0,0 +1,10 @@
+﻿using FluentResults;
+using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    internal interface IQuery<TResponse> : IRequest<Result<TResponse>>
+        where TResponse : notnull
+    {
+    }
+}
